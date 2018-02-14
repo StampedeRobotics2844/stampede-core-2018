c67d16e0a16a269c40937e4881561bed4fc18685
Developer 1 <developer@gmail.com>
2018-02-13 17:03:06 -0700

    Motors

--- robot.py
+++ robot.py
@@ -8,6 +8,7 @@ bfhsroboticsclub@gmail.com
 import wpilib
 import wpilib.drive
 import logging
+import portmap
 
 from networktables import NetworkTables
 from robotpy_ext.autonomous import AutonomousModeSelector
@@ -24,8 +25,14 @@ class StampedeRobot(wpilib.IterativeRobot):
         self.drive_r_motor = None
         self.drive_l_motor = None
 
+        self.claw_rintake_motor = None
+        self.claw_lintake_motor = None
+
+        self.elevator_motor = None
+
         self.left_stick = None
         self.right_stick = None
+        
 
         self.drive = None
 
@@ -41,19 +48,24 @@ class StampedeRobot(wpilib.IterativeRobot):
         wpilib.CameraServer.launch()
 
         #Initalizing drive motors
-        self.drive_r_motor = wpilib.Spark(9)
-        self.drive_l_motor = wpilib.Spark(8)
+        self.drive_l_motor = wpilib.Spark(portmap.motors.left_drive)
+        self.drive_r_motor = wpilib.Spark(portmap.motors.right_drive)
+
+        self.claw_lintake_motor = wpilib.Victor(portmap.motors.left_intake)
+        self.claw_rintake_motor = wpilib.Victor(portmap.motors.right_intake)
+
+        self.elevator_motor = wpilib.Spark(portmap.motors.elevator)
 
         # initialize drive
         self.drive = wpilib.drive.DifferentialDrive(self.drive_l_motor, self.drive_r_motor)
         self.drive.setExpiration(0.1)
 
         # joysticks 1 & 2 on the driver station
-        self.left_stick = wpilib.Joystick(0)
-        self.right_stick = wpilib.Joystick(1)
+        self.left_stick = wpilib.Joystick(portmap.joysticks.left_joystick)
+        self.right_stick = wpilib.Joystick(portmap.joysticks.right_joystick)
 
         # initialize gyro
-        self.gyro = wpilib.ADXRS450_Gyro()
+        #self.gyro = wpilib.ADXRS450_Gyro()
 
         # initialize Accelerometer
         #self.accel = wpilib.ADXL345_I2C(wpilib.I2C.Port.kMXP,
@@ -84,7 +96,24 @@ class StampedeRobot(wpilib.IterativeRobot):
         '''Called every 20ms in teleoperated mode'''
         
         try:
+
+            if self.left_stick.getTrigger():
+                self.claw_lintake_motor.set(1)
+                self.claw_rintake_motor.set(1)
+            elif self.right_stick.getTrigger():
+                self.claw_lintake_motor.set(-1)
+                self.claw_rintake_motor.set(-1)
+            else:
+                self.claw_lintake_motor.set(0)
+                self.claw_rintake_motor.set(0)
+
+            if self.left_stick.getTrigger():
+                self.elevator_motor.set(1)
+            else:
+                self.elevator_motor.set(0)
+            
             self.drive.tankDrive(self.left_stick.getY(), self.right_stick.getY(), True)
+
         except:
             if not self.isFMSAttached():
                 raise
