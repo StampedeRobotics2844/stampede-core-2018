136a2002b810792486b3e2c40225689bae968489
Developer 1 <developer@gmail.com>
2018-02-14 15:38:32 -0700

    addded portmap

---
+++ portmap.py
@@ -0,0 +1,20 @@
+class PortMap:
+    pass
+
+motors = PortMap()
+
+motors.left_drive = 8
+motors.right_drive = 9
+motors.left_intake = 3
+motors.right_intake = 2
+motors.claw = 5
+motors.climb = 0
+motors.elevator = 1
+
+joysticks = PortMap()
+
+joysticks.left_joystick = 0
+joysticks.right_joystick = 1
+
+joysticks.button_intake = 1
+joysticks.button_outtake = 1
